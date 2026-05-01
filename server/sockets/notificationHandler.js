99fbc0dbb41083ae7e0614811c53235362e3a35f
phanthingochan <ngochan2005blislife@gmail.com>
2026-05-01 15:20:20 +0700

    refactor: start removing monolothic architecture's trace

--- server/sockets/notificationHandler.js
+++
@@ -1,5 +0,0 @@
-export default function registerNotificationHandlers(io) {
-  // Notification handlers are already registered in index.js
-  // This function is kept for future extension if needed
-  console.log("Notification handlers registered");
-}
--- server/utils/time.js
+++
@@ -1,3 +0,0 @@
-export const timeToMinutes = ({ time }) => {
-    console.log("timeToMinutes received:", time);
-};
